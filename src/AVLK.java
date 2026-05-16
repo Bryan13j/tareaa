f5ec63c74d32768516c63a83795778d222206c2e
Bryan <bryanscoty1234@gmail.com>
2026-05-16 17:23:48 -0400

    esto hice por el momento

--- src/AVLK.java
+++ src/AVLK.java
@@ -4,47 +4,73 @@ class AVLK {
     AVLK() {
         laRaiz = null;
     }
+
     private boolean masAlto;
 
     //insertar un nodo, con metodo recursivo
     public void Insertar(int elemento) {
         laRaiz = InsertaenAVL(laRaiz, elemento);
-        masAlto=false;
+        masAlto = false;
     }
-    private NodoAVLK InsertaenAVL(NodoAVLK a, int elemento){
-        if(a == null){ masAlto = true; return new NodoAVLK(elemento,null,null); }
-        else if (elemento < a.elemento){
-            a.lchild= InsertaenAVL(a.lchild,elemento);
-            if(masAlto)
-                switch(a.balan){
-                    case 1: masAlto = false;a=balanceaIzq(a); break;
-                    case 0: a.balan = 1; break;
-                    case -1: masAlto=false; a.balan=0; break;
+
+    private NodoAVLK InsertaenAVL(NodoAVLK a, int elemento) {
+        if (a == null) {
+            masAlto = true;
+            return new NodoAVLK(elemento, null, null);
+        } else if (elemento < a.elemento) {
+            a.lchild = InsertaenAVL(a.lchild, elemento);
+            if (masAlto)
+                switch (a.balan) {
+                    case 1:
+                        masAlto = false;
+                        a = balanceaIzq(a);
+                        break;
+                    case 0:
+                        a.balan = 1;
+                        break;
+                    case -1:
+                        masAlto = false;
+                        a.balan = 0;
+                        break;
                 }
-        }
-        else { a.rchild= InsertaenAVL(a.rchild,elemento);
-            if(masAlto)
-                switch(a.balan){
-                    case 1: masAlto = false; a.balan = 0; break;
-                    case 0: a.balan = -1; break;
-                    case -1: masAlto=false; a= balanceaDer(a); break;
+        } else {
+            a.rchild = InsertaenAVL(a.rchild, elemento);
+            if (masAlto)
+                switch (a.balan) {
+                    case 1:
+                        masAlto = false;
+                        a.balan = 0;
+                        break;
+                    case 0:
+                        a.balan = -1;
+                        break;
+                    case -1:
+                        masAlto = false;
+                        a = balanceaDer(a);
+                        break;
                 }
         }
         return a;
     }
 
     //balancear el avl si esta desbalanceado hacia la derecha
-    private NodoAVLK balanceaDer(NodoAVLK a){
-        if(a.rchild.balan==-1){
-            a.balan=a.rchild.balan=0;
+    private NodoAVLK balanceaDer(NodoAVLK a) {
+        if (a.rchild.balan == -1) {
+            a.balan = a.rchild.balan = 0;
             a = roteIzq(a);
-        }
-        else
-        {
-            switch(a.rchild.lchild.balan)
-            { case 1: a.balan = 0; a.rchild.balan = -1; break;
-                case 0: a.balan=a.rchild.balan=0; break;
-                case -1: a.balan = 1; a.rchild.balan=0; break;
+        } else {
+            switch (a.rchild.lchild.balan) {
+                case 1:
+                    a.balan = 0;
+                    a.rchild.balan = -1;
+                    break;
+                case 0:
+                    a.balan = a.rchild.balan = 0;
+                    break;
+                case -1:
+                    a.balan = 1;
+                    a.rchild.balan = 0;
+                    break;
             }
             a.rchild.lchild.balan = 0;
             a = roteDerIzq(a);
@@ -55,46 +81,82 @@ class AVLK {
     //balancear el avl si esta desbalanceado hacia la izquierda
     private NodoAVLK balanceaIzq(NodoAVLK a) {
         boolean masBajo = true;
-        switch(a.balan){
-            case 1: if(a.lchild.balan != -1) {
-                a = roteDer(a);
-                if(a.balan == 0){
-                    a.balan = -1;
-                    a.rchild.balan=1;
-                    masBajo = false;
+        switch (a.balan) {
+            case 1:
+                if (a.lchild.balan != -1) {
+                    a = roteDer(a);
+                    if (a.balan == 0) {
+                        a.balan = -1;
+                        a.rchild.balan = 1;
+                        masBajo = false;
+                    } else {
+                        a.balan = a.rchild.balan = 0;
+                    }
+
+                } else {
+                    a = roteIzqDer(a);
+                    a.rchild.balan = a.balan == 1 ? (short) -1 : (short) 0;
+                    a.lchild.balan = a.balan == -1 ? (short) 1 : (short) 0;
+                    a.balan = 0;
                 }
-                else
-                    a.balan = a.rchild.balan = 0;
-            }
-            else {
-                a = roteIzqDer(a);
-                a.rchild.balan = a.balan == 1 ? (short) -1 : (short) 0;
-                a.lchild.balan = a.balan == -1 ? (short) 1 : (short) 0;
-                a.balan = 0;
-            }
                 break;
-            case 0: a.balan = 1;
+            case 0:
+                a.balan = 1;
                 masBajo = false;
                 break;
-            case -1: a.balan=0;
+            case -1:
+                a.balan = 0;
                 break;
         }
         return a;
     }
+
+    //elimina al nodo y lo balancea para no perder el AVL
+    private boolean masBajo;
+    public void Eliminar(int elemento){
+        laRaiz=EliminaenAVL(laRaiz, elemento);
+        masBajo = false;
+    }
+    private NodoAVLK EliminaenAVL(NodoAVLK a, int elemento){
+        if(elemento == a.elemento) {
+            if(a.lchild == null && a.rchild == null){
+                masBajo = true;
+                return null;
+            }
+            else if(a.lchild == null){
+                masBajo=true;
+                return a.rchild;
+            }
+            else {
+                a.elemento = BuscaMayorIzq(a.lchild);
+                a.lchild=EliminaenAVL(a.lchild,a.elemento);
+
+            }
+        }
+        else if(a.elemento > elemento){
+            a.lchild = EliminaenAVL(a.lchild,elemento);
+
+        }
+        else {
+            a.rchild = EliminaenAVL(a.rchild,elemento);
+            if(masBajo) a = balanceaIzq(a);
+        }
+        return a;
+    }
+
     //busca si el elemento k pertenece al AVLK
-    public boolean Buscar(int k){
+    public boolean Buscar(int k) {
         return Buscar(laRaiz, k);
     }
-    private boolean Buscar(NodoAVLK a, int k){
-        if (a == null){
+    private boolean Buscar(NodoAVLK a, int k) {
+        if (a == null) {
             return false;
         }
         if (k == a.elemento) {
             return true;
-        }
-        else if (k < a.elemento){
+        } else if (k < a.elemento) {
             return Buscar(a.rchild, k);
-        } else{
+        } else {
             return Buscar(a.lchild, k);
         }
     }
@@ -103,19 +165,20 @@ class AVLK {
     private int contador = 0;
     private int resutado = -1;
     private int Iesimo(int i) {
-        contador =0;
-        resutado =-1;
+        contador = 0;
+        resutado = -1;
         recorrer(laRaiz, i);
         return resutado;
     }
-    private void recorrer(NodoAVLK r, int i){
-        if(r == null || contador >= i){
+
+    private void recorrer(NodoAVLK r, int i) {
+        if (r == null || contador >= i) {
             return;
         }
 
         recorrer(r.rchild, i);
         contador++;
-        if(contador == i){
+        if (contador == i) {
             resutado = r.elemento;
             return;
         }
@@ -127,6 +190,7 @@ class AVLK {
         a.rchild = roteDer(a.rchild);
         return roteIzq(a);
     }
+
     //rotacion doble hacia la derecha
     private NodoAVLK roteIzqDer(NodoAVLK a) {
         a.lchild = roteIzq(a.lchild);
@@ -134,22 +198,20 @@ class AVLK {
     }
 
     //rotacion a la izquierda
-    private NodoAVLK roteIzq(NodoAVLK a){
+    private NodoAVLK roteIzq(NodoAVLK a) {
         NodoAVLK temp = a.rchild;
-        a.rchild= temp.lchild;
-        temp.lchild= a;
+        a.rchild = temp.lchild;
+        temp.lchild = a;
         return temp;
     }
 
     //rotacion a la derecha
     private NodoAVLK roteDer(NodoAVLK a) {
         NodoAVLK temp = a.lchild;
-        a.lchild= temp.rchild;
-        temp.rchild= a;
+        a.lchild = temp.rchild;
+        temp.rchild = a;
         return temp;
     }
-
-
 
 
 }
